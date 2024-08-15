c87384b7aa58486b4585abb67142055259180cf6
miguecarmona12 <carmonamiguel1212@gmail.com>
2024-08-14 22:16:40 -0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "https://miguecarmona12.github.io/Plants",
+  base: "https://miguecarmona12.github.io/Plants",
 });
